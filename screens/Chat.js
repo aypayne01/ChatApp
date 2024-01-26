3b4c1b437d77cb18c9e82b5e3e65fe51d2d578d0
Alex Payne <alexpayne@lawn-128-61-54-64.lawn.gatech.edu>
2024-01-26 17:38:27 -0500

    implemented chat

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,29 +1,85 @@
-import React, { useEffect, useState } from "react";
-import { GiftedChat } from "react-native-gifted-chat";
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback
+} from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
 
-export default function Chat() {
+import { auth, database } from '../config/firebase';
+
+export default function Chat({ navigation }) {
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    // Fetch or set initial messages here
-    const initialMessages = [
-      {
-        _id: 1,
-        text: "Hello!",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-        },
-      },
-    ];
-
-    setMessages(initialMessages);
-  }, []); // Ensure the dependency array is empty to run the effect only once
-
-  const onSend = (newMessages = []) => {
-    setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
+const onSignOut = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
   };
 
-  return <GiftedChat messages={messages} onSend={onSend} />;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10
+          }}
+          onPress={onSignOut}
+        >
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      )
+    });
+  }, [navigation]);
+
+  useEffect(() => {
+    const collectionRef = collection(database, 'chats');
+    const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      setMessages(
+        querySnapshot.docs.map(doc => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user
+        }))
+      );
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+
+const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    const { _id, createdAt, text, user } = messages[0];    
+    addDoc(collection(database, 'chats'), {
+      _id,
+      createdAt,
+      text,
+      user
+    });
+  }, []);
+
+  return (
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={true}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: auth?.currentUser?.email,
+        avatar: 'https://i.pravatar.cc/300'
+      }}
+    />
+  );
 }
